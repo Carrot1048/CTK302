ac50b652a059dfbe491d5ee20ff2b51ca67003ed
Othniel <carrot1048@gmail.com>
2022-01-25 11:34:24 -0600

    Fonts

--- Unit1/Fonts/sketch.js
+++ Unit1/Fonts/sketch.js
@@ -1,7 +1,22 @@
+let font1 ;
+let font2 ;
+
 function setup() {
   createCanvas(500, 500);
+
+  font1 = loadFont("Assets/Courgette.ttf");
+  font2 = loadFont("Assets/Rubik.ttf");
+
+  textAlign(CENTER);
 }
 
 function draw() {
+  background(0);
 
+  textSize(48);
+  fill(94, 71, 186);
+  textFont(font1);
+  text("Hola,", width/2, 100);
+  textFont(font2);
+  text("Me llamo Othniel.", width/2, 200);
 }
