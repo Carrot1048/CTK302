befafe018c85254da83e566465392416efb730d2
Othniel <carrot1048@gmail.com>
2022-01-28 01:00:39 -0600

    Begin

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,6 +1,7 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 600);
   rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -10,8 +11,21 @@ function draw() {
     fill(0);
   }else{
     background(0);
-    fill(61, 133, 121);
+    fill(82, 49, 20);
+    rect(width/2, 400, 80, 160);
+    push();
+    fill(58, 31, 77);
+    rect(width/2, 370, 80, 100);
+    pop();
+    push();
+    translate(460, 400);
+    rotate(15);
+    rect(0, 0, 40, 160);
+    pop();
+    push();
+    translate(540, 400);
+    rotate(-15);
+    rect(0, 0, 40, 160);
+    pop();
   }
-
-  rect(width/2,height/2,200,100);
 }
