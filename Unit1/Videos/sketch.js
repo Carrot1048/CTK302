a35d6098e6c6520679847e5fe836950be4a3a0fe
Othniel <carrot1048@gmail.com>
2022-02-01 13:26:35 -0600

    video

---
+++ Unit1/Videos/sketch.js
@@ -0,0 +1,32 @@
+let winter ;
+let playing = false ;
+
+function setup() {
+  createCanvas(500,500);
+  imageMode(CENTER);
+  textAlign(CENTER);
+  // winter = createVideo("Assets/Winter.MOV", vidLoad);
+  // winter.size(500, 500);
+  winter.hide();
+}
+
+function draw() {
+  image(winter, width/2, height/2, 500, 500);
+  text("Hello Winter", width/2, height/2);
+}
+
+function mouseReleased() {
+  if (!playing) {
+    winter.play();
+    playing = true;
+  }
+  else {
+    winter.pause();
+    playing = false;
+  }
+}
+
+function vidLoad(){
+  winter.loop();
+  winter.volume(0);
+}
