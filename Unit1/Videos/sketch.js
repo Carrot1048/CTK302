8ed4953ed08e5ad92c3c6869b3065caa5b52cd83
Othniel <carrot1048@gmail.com>
2022-02-01 13:28:14 -0600

    Need different video

--- Unit1/Videos/sketch.js
+++ Unit1/Videos/sketch.js
@@ -7,26 +7,26 @@ function setup() {
   textAlign(CENTER);
   // winter = createVideo("Assets/Winter.MOV", vidLoad);
   // winter.size(500, 500);
-  winter.hide();
+  // winter.hide();
 }
 
 function draw() {
-  image(winter, width/2, height/2, 500, 500);
+  // image(winter, width/2, height/2, 500, 500);
   text("Hello Winter", width/2, height/2);
 }
 
-function mouseReleased() {
-  if (!playing) {
-    winter.play();
-    playing = true;
-  }
-  else {
-    winter.pause();
-    playing = false;
-  }
-}
-
-function vidLoad(){
-  winter.loop();
-  winter.volume(0);
-}
+// function mouseReleased() {
+//   if (!playing) {
+//     winter.play();
+//     playing = true;
+//   }
+//   else {
+//     winter.pause();
+//     playing = false;
+//   }
+// }
+//
+// function vidLoad(){
+//   winter.loop();
+//   winter.volume(0);
+// }
