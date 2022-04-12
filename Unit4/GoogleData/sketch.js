fcd066cc5d4dfb5074fcc62ad12dc129debc85ca
Othniel <carrot1048@gmail.com>
2022-04-12 18:07:33 -0500

    Images and Personalization

--- Unit4/GoogleData/sketch.js
+++ Unit4/GoogleData/sketch.js
@@ -1,5 +1,6 @@
 var bubbles = [];
 let url = "";
+let season1, season2, season3, season4;
 
 function setup() {
   //let key = "1xG5lzBtJV3gK61ZE_qdku3ms9-pCJqwl0T8RVHI11m0"; // this is KEY of the URL from the sheet
@@ -7,6 +8,10 @@ function setup() {
 
   url = "https://opensheet.vercel.app/" + key + "/Form+Responses+1"; // here I'm making the string for loadJSON.
 
+  season1 = loadImage("Assets/Fall.png");
+  season2 = loadImage("Assets/Winter.png");
+  season3 = loadImage("Assets/Spring.png");
+  season4 = loadImage("Assets/Summer.png");
   loadJSON(url, gotData);
 
   // Regular setup code we usually have
@@ -14,6 +19,7 @@ function setup() {
   textAlign(CENTER);
   ellipseMode(CENTER);
   rectMode(CENTER);
+  imageMode(CENTER);
 }
 
 // The data comes back as an array of objects
@@ -34,7 +40,7 @@ function gotData(data) {
 }
 
 function draw() {
-  background("blue");
+  background(99, 88, 88);
 
   // // iterate through the bubbles and display the objects!
   for (let i = 0; i < bubbles.length; i++) {
@@ -53,18 +59,58 @@ class Bubble {
     this.name = name;
     this.pos = createVector(random(width), random(height));
     this.vel = createVector(random(2, 5), random(-1, 1));
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
   }
 
   display() {
-    stroke("red");
-    noFill();
-    ellipse(this.pos.x, this.pos.y+10, 120, 120);
-    fill("white");
+    // stroke("white");
+    //Filling the shapes based on the person's favorite color
+    fill('black');
+    if(this.color=="red"||this.color=="Red"){
+      fill('red');
+    }
+    if(this.color=="blue"||this.color=="Blue"){
+      fill('blue');
+    }
+    if(this.color=="yellow"||this.color=="Yellow"){
+      fill(158, 154, 43);
+    }
+    if(this.color=="purple"||this.color=="Purple"){
+      fill(67, 18, 120);
+    }
+    if(this.color=="green"||this.color=="Green"){
+      fill(35, 120, 40);
+    }
+    //If siblings is even, then shape is square, if odd, then shape is circle
+    if(this.siblings%2==0){
+      rect(this.pos.x, this.pos.y+20, 120, 120);
+    }else{
+      ellipse(this.pos.x, this.pos.y+10, 120, 120);
+    }
+    //Image based on favorite season
+    if(this.season=="Fall"){
+      image(season1, this.pos.x, this.pos.y);
+    }
+    if(this.season=="Winter"){
+      image(season2, this.pos.x, this.pos.y);
+    }
+    if(this.season=="Spring"){
+      image(season3, this.pos.x, this.pos.y);
+    }
+    if(this.season=="Summer"){
+      image(season4, this.pos.x, this.pos.y);
+    }
+    push();
+    stroke("black");
+    fill(this.r, this.g, this.b);
     text(
       this.siblings + "\n" + this.color + "\n" + this.animal + "\n" + this.season + "\n" + this.name,
       this.pos.x,
       this.pos.y
     );
+    pop();
 
     this.pos.add(this.vel);
     if (this.pos.x > width) this.pos.x = 0;
